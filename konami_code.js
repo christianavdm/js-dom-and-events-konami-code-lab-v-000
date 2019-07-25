87f599233509b7eaa5417018b816c75f819a5934
christianavdm <christianavdm@gmail.com>
2019-07-25 14:27:37 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,41 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0; 
 
+/* const main = document.getElementById('main');
+
+main.addEventListener('keydown', function init(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}); */
+
+function init() {
+  let index = 0; 
+
+  document.addEventListener('keydown', function init(e) {
+    const key = parseInt(e.detail || e.which);
+   
+    if (key === code[index]) {
+      index++;
+   
+      if (index === code.length) {
+        alert("Congratulations");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
